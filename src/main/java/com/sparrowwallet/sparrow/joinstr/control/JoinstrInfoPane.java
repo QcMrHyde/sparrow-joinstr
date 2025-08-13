60ca3ff274306645d7a6e6122667fbb9f657be0f
QcMrHyde <hc1800@hotmail.com>
2025-08-13 13:46:09 -0400

    Fix joinstr menu items glitch and CSS for light mode

--- src/main/java/com/sparrowwallet/sparrow/joinstr/control/JoinstrInfoPane.java
+++ src/main/java/com/sparrowwallet/sparrow/joinstr/control/JoinstrInfoPane.java
@@ -2,7 +2,6 @@ package com.sparrowwallet.sparrow.joinstr.control;
 
 import com.sparrowwallet.sparrow.joinstr.JoinstrPool;
 
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -19,13 +18,14 @@ public class JoinstrInfoPane extends VBox {
     private Label denominationValueLabel;
 
     public JoinstrInfoPane() {
-        setStyle("-fx-background-color: #222222; -fx-padding: 15;");
+        getStylesheets().add(getClass().getResource("../../darktheme.css").toExternalForm());
+        getStyleClass().add("joinstr-infopane");
         setSpacing(10);
     }
 
     public void initInfoPane() {
         titleLabel = new Label("Selected Pool Details");
-        titleLabel.setStyle("-fx-font-size: 16px; -fx-text-fill: white; -fx-font-weight: bold;");
+        titleLabel.getStyleClass().add("sub-title");
         getChildren().add(titleLabel);
 
         GridPane detailsGrid = new GridPane();
@@ -39,19 +39,16 @@ public class JoinstrInfoPane extends VBox {
         detailsGrid.getColumnConstraints().addAll(column1, column2);
 
         relayLabel = new Label("Relay:");
-        relayLabel.setStyle("-fx-text-fill: #aaaaaa;");
+        relayLabel.getStyleClass().add("text-grey");
         relayValueLabel = new Label();
-        relayValueLabel.setStyle("-fx-text-fill: white;");
 
         pubkeyLabel = new Label("Pubkey:");
-        pubkeyLabel.setStyle("-fx-text-fill: #aaaaaa;");
+        relayLabel.getStyleClass().add("text-grey");
         pubkeyValueLabel = new Label();
-        pubkeyValueLabel.setStyle("-fx-text-fill: white;");
 
         denominationLabel = new Label("Denomination:");
-        denominationLabel.setStyle("-fx-text-fill: #aaaaaa;");
+        relayLabel.getStyleClass().add("text-grey");
         denominationValueLabel = new Label();
-        denominationValueLabel.setStyle("-fx-text-fill: white;");
 
         detailsGrid.add(relayLabel, 0, 0);
         detailsGrid.add(relayValueLabel, 1, 0);
