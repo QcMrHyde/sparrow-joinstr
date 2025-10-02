eaa1dc7606da9bdac97ef27276a82f44ba6554c8
QcMrHyde <hc1800@hotmail.com>
2025-10-01 22:26:10 -0400

    Remove unused import

--- src/main/java/com/sparrowwallet/sparrow/joinstr/OtherPoolsController.java
+++ src/main/java/com/sparrowwallet/sparrow/joinstr/OtherPoolsController.java
@@ -7,7 +7,6 @@ import com.sparrowwallet.sparrow.joinstr.control.JoinstrPoolList;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
