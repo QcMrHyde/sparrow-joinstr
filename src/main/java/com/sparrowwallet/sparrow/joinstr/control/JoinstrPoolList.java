5aa655ea1d7b377addf6880ccb0c92f1a4f74ca5
/dev/fd0 <alicexbt@protonmail.com>
2025-12-13 03:55:14 +0530

    manage JoinPoolHandler lifecycle to stop listeners properly

--- src/main/java/com/sparrowwallet/sparrow/joinstr/control/JoinstrPoolList.java
+++ src/main/java/com/sparrowwallet/sparrow/joinstr/control/JoinstrPoolList.java
@@ -30,6 +30,7 @@ import java.util.function.Consumer;
 import com.sparrowwallet.sparrow.joinstr.JoinPoolHandler;
 import javafx.application.Platform;
 import nostr.base.PublicKey;
+import java.util.HashMap;
 
 public class JoinstrPoolList extends VBox {
 
@@ -37,6 +38,7 @@ public class JoinstrPoolList extends VBox {
     private ObservableList<JoinstrPool> poolData;
     private FilteredList<JoinstrPool> filteredData;
     private Consumer<JoinstrPool> onPoolSelectedListener;
+    private Map<String, JoinPoolHandler> poolHandlers = new HashMap<>();
 
     public JoinstrPoolList() {
         initialize();
@@ -163,6 +165,9 @@ public class JoinstrPoolList extends VBox {
 
                         joinButton.setOnAction(event -> {
                             JoinstrPool pool = getTableView().getItems().get(getIndex());
+
+                            stopPoolHandler(pool.getPubkey());
+                            
                             Identity identity = Identity.generateRandomIdentity();
                             String pubkey = identity.getPublicKey().toString();
                             QRDisplayDialog qrDialog = new QRDisplayDialog(pubkey);
@@ -197,6 +202,8 @@ public class JoinstrPoolList extends VBox {
                                 });
                             });
                             handler.startListeningForCredentials();
+
+                            poolHandlers.put(pool.getPubkey(), handler);
                         });
                     }
 
@@ -224,7 +231,22 @@ public class JoinstrPoolList extends VBox {
         poolTableView.getSelectionModel().select(poolToSelect);
     }
     public void clearPools() {
+        stopAllHandlers();
         poolData.clear();
+    }
+
+    public void stopPoolHandler(String poolPubkey) {
+        JoinPoolHandler handler = poolHandlers.remove(poolPubkey);
+        if (handler != null) {
+            handler.stop();
+        }
+    }
+
+    public void stopAllHandlers() {
+        for (JoinPoolHandler handler : poolHandlers.values()) {
+            handler.stop();
+        }
+        poolHandlers.clear();
     }
 
     public void filterPools(String searchText) {
